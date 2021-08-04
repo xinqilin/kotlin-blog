5bf87785494040234ef04c95ae266b8ad803f11b
BillLin1209 <zzx123bill@gmail.com>
2021-08-04 21:07:28 +0800

    init project

---
+++ src/test/kotlin/com/bill/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bill.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
