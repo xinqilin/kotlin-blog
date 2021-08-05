575c4ba1c47fc9d7c9943ac151efd6941e764d57
BillLin1209 <zzx123bill@gmail.com>
2021-08-05 20:03:34 +0800

    admin and blog table

--- src/main/kotlin/com/bill/blog/entity/AdminUser.kt
+++ src/main/kotlin/com/bill/blog/entity/AdminUser.kt
@@ -1,7 +1,33 @@
 package com.bill.blog.entity
 
+import javax.persistence.*
+
 /**
  * @author Bill.Lin on 2021/8/4
  */
+
+@Entity
+@Table("admin_user")
 class AdminUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var adminUserId: Int? = null
+
+    var loginUserName: String = ""
+        set(loginUserName) {
+            field = loginUserName?.trim { it <= ' ' }
+        }
+
+    var loginPassword: String = ""
+        set(loginPassword) {
+            field = loginPassword?.trim { it <= ' ' }
+        }
+
+    var nickName: String = ""
+        set(nickName) {
+            field = nickName?.trim { it <= ' ' }
+        }
+
+    var locked: Byte = 0
 }
--- src/main/kotlin/com/bill/blog/entity/Blog.kt
+++ src/main/kotlin/com/bill/blog/entity/Blog.kt
@@ -1,7 +1,64 @@
 package com.bill.blog.entity
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import java.util.*
+import javax.persistence.*
+
 /**
  * @author Bill.Lin on 2021/8/4
  */
+@Entity
+@Table("blog")
 class Blog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var blogId: Long = 0
+
+    var blogCategoryId: Int = 0
+
+    var blogTitle: String = ""
+        set(blogTitle) {
+            field = blogTitle?.trim { it <= ' ' }
+        }
+
+    var blogSubUrl: String = ""
+        set(blogSubUrl) {
+            field = blogSubUrl?.trim { it <= ' ' }
+        }
+
+    var blogCoverImage: String = ""
+        set(blogCoverImage) {
+            field = blogCoverImage?.trim { it <= ' ' }
+        }
+
+    var blogCategoryName: String = ""
+        set(blogCategoryName) {
+            field = blogCategoryName?.trim { it <= ' ' }
+        }
+
+    var blogTags: String = ""
+        set(blogTags) {
+            field = blogTags?.trim { it <= ' ' }
+        }
+
+    var blogStatus: Byte = 0
+
+    var blogViews: Long = 0
+
+    var enableComment: Byte = 0
+
+    var isDeleted: Byte = 0
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    var createTime: Date = Date()
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    var updateTime: Date = Date()
+
+    var blogContent: String = ""
+        set(blogContent) {
+            field = blogContent?.trim { it <= ' ' }
+        }
+
 }
