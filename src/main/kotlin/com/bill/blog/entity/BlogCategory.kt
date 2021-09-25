7317ed0bf842e14c7c2457d2bd9d3901ec95a01a
BillLin1209 <zzx123bill@gmail.com>
2021-09-25 23:46:05 +0800

    add blogCategory entity

--- src/main/kotlin/com/bill/blog/entity/BlogCategory.kt
+++ src/main/kotlin/com/bill/blog/entity/BlogCategory.kt
@@ -1,26 +1,43 @@
 package com.bill.blog.entity
 
-import com.bill.blog.vo.SoftDeletedModel
 import com.fasterxml.jackson.annotation.JsonFormat
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.util.*
 import javax.persistence.*
 
 /**
  * @author Bill.Lin on 2021/8/4
  */
-//@Entity
-//@Table(name = "blog_category")
+@Entity
+@Table(name = "blog_category")
+@EntityListeners(AuditingEntityListener::class)
 data class BlogCategory(
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         var categoryId: Int = 0,
+
+        @Column(name = "category_name", columnDefinition = "varchar(100) NOT NULL DEFAULT ''")
         var categoryName: String = "",
+
+        @Column(name = "category_icon", columnDefinition = "varchar(500) NOT NULL DEFAULT ''")
         var categoryIcon: String = "",
+
+        @Column(name = "category_rank", columnDefinition = "integer NOT NULL DEFAULT 0")
         var categoryRank: Int = 0,
+
+        @Column(name = "deleted", columnDefinition = "TINYINT(1) DEFAULT 0", nullable = false)
         var isDeleted: Byte = 0,
+
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+        @CreatedDate
+        @Column(name = "create_time", columnDefinition = "DATETIME", updatable = false)
         var createTime: Date = Date(),
+
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+        @LastModifiedDate
+        @Column(name = "update_time", columnDefinition = "DATETIME")
         var updateTime: Date = Date()
 )
